713ecb5fa8abfee062b9e3f25fff5e3820ddfdbe
xchang4 <changxiaowen1992@gmail.com>
2017-10-29 20:19:23 -0700

    hw0

---
+++ 6.001/lecture/ps0/ps0_answer.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+
+x = int(input("Enter number x:"))
+y = int(input("Enter number y:"))
+print('x**y= ', x**y)
+print('log(x)= ', np.log2(x))
